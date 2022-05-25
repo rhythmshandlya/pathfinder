1bbd6d2fcd79c21f619faee0a1ca04f3ee1cc21c
Rhythm Shandlya <armaanbgp@gmail.com>
2022-05-25 15:43:37 +0530

    Updates

---
+++ precache-manifest.b3b50034e38b0935b2b7313079b9c72e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abeccd1720d54de6a147bc5ad82e003",
+    "url": "/pathfinder/index.html"
+  },
+  {
+    "revision": "1865795f71e11b72f915",
+    "url": "/pathfinder/static/css/main.0d3bcb04.chunk.css"
+  },
+  {
+    "revision": "0148385edcaafff07248",
+    "url": "/pathfinder/static/js/2.0db164ae.chunk.js"
+  },
+  {
+    "revision": "1865795f71e11b72f915",
+    "url": "/pathfinder/static/js/main.a008de63.chunk.js"
+  },
+  {
+    "revision": "0e5b37ef767bd06e1b89",
+    "url": "/pathfinder/static/js/runtime-main.dba5c086.js"
+  }
+]);
